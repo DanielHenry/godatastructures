79d0b5fed349b7a1c43ddbebf56f92bac3a84aa8
DanielHenry <daniel.henry.39@gmail.com>
2019-04-25 12:41:28 +0700

    Finish implement Push() function in push.go at linkedlist package

--- pkg/linkedlist/push.go
+++ pkg/linkedlist/push.go
@@ -5,7 +5,13 @@ func (ll *LinkedList) Push(item Item) {
 	temp := new(Node)
 	temp.val = item
 	temp.next = nil
-	ll.head = temp
-	ll.tail = temp
-	ll.size = 1
+	if ll.head == nil && ll.tail == nil {
+		ll.head = temp
+		ll.tail = temp
+		ll.size = 1
+	} else {
+		ll.tail.next = temp
+		ll.tail = temp
+		ll.size++
+	}
 }
