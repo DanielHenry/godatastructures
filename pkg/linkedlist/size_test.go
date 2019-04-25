ea17a6615b8233f38f9ea0fdb595ca92a5aa659c
DanielHenry <daniel.henry.39@gmail.com>
2019-04-25 12:23:15 +0700

    Update size_test.go

--- pkg/linkedlist/size_test.go
+++ pkg/linkedlist/size_test.go
@@ -16,12 +16,8 @@ func TestSizeAfterPushOneValue(t *testing.T) {
 func TestSizeAfterPushTwoValue(t *testing.T) {
 	ll := New()
 	ll.Push(3)
-	size := ll.Size()
-	if size != 1 {
-		t.Errorf("wrong size, expected 1 and got %d", size)
-	}
 	ll.Push(3)
-	size = ll.Size()
+	size := ll.Size()
 	if size != 2 {
 		t.Errorf("wrong size, expected 2 and got %d", size)
 	}
@@ -30,17 +26,9 @@ func TestSizeAfterPushTwoValue(t *testing.T) {
 func TestSizeAfterPushThreeValue(t *testing.T) {
 	ll := New()
 	ll.Push(3)
+	ll.Push(3)
+	ll.Push(3)
 	size := ll.Size()
-	if size != 1 {
-		t.Errorf("wrong size, expected 1 and got %d", size)
-	}
-	ll.Push(3)
-	size = ll.Size()
-	if size != 2 {
-		t.Errorf("wrong size, expected 2 and got %d", size)
-	}
-	ll.Push(3)
-	size = ll.Size()
 	if size != 3 {
 		t.Errorf("wrong size, expected 3 and got %d", size)
 	}
