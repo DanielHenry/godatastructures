cd11888c87640da0d0e4491aec376fe9e2dd823c
DanielHenry <daniel.henry.39@gmail.com>
2019-04-29 05:53:37 +0700

    Add popback_test.go

---
+++ pkg/linkedlist/popback_test.go
@@ -0,0 +1,39 @@
+package linkedlist
+
+import (
+    "testing"
+)
+
+func TestPushOneValueAndPopBackOneValue(t *testing.T) {
+	ll := New()
+	ll.Push(3)
+	ll.PopBack()
+	size := ll.Size()
+	if size != 0 {
+		t.Errorf("wrong size, expected 1 and got %d", size)
+	}
+}
+
+func TestPushTwoValueAndPopBackOneValue(t *testing.T) {
+	ll := New()
+	ll.Push(3)
+	ll.Push(3)
+	ll.PopBack()
+	size := ll.Size()
+	if size != 1 {
+		t.Errorf("wrong size, expected 2 and got %d", size)
+	}
+}
+
+func TestPushThreeValueAndPopBackOneValue(t *testing.T) {
+	ll := New()
+	ll.Push(3)
+	ll.Push(3)
+	ll.Push(3)
+	ll.PopBack()
+	size := ll.Size()
+	if size != 2 {
+		t.Errorf("wrong size, expected 3 and got %d", size)
+	}
+}
+
