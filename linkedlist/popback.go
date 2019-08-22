aa58a08e69a081ed156d0f1e70cfd614794aee58
Daniel Henry <daniel.henry@tokopedia.com>
2019-08-23 01:18:22 +0700

    Fix iter undefined variable in PopBack() function

--- linkedlist/popback.go
+++ linkedlist/popback.go
@@ -1,17 +1,17 @@
 package linkedlist
 
-
 func (ll *LinkedList) PopBack() {
 	if ll.size == 0 {
 		ll.head = nil
-		ll.tail =  nil
+		ll.tail = nil
 		ll.size = 0
 	} else if ll.size == 1 {
 		ll.head = nil
-		ll.tail =  nil
+		ll.tail = nil
 		ll.size = 0
 	} else {
-		for iter := ll.head; iter.next != ll.tail; iter = iter.next {
+		iter := ll.head
+		for ; iter.next != ll.tail; iter = iter.next {
 			continue
 		}
 		ll.tail = iter
